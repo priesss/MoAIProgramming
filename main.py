1abc070eb088e10f1164702f7659038187d70fec
Stefan Balle <stefan@Stefans-MacBook-Pro.local>
2018-01-03 22:12:01 +0100

    Read grid files

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-import argparse #To handle argumets
+import inspect, os #Imports
+import numpy as np
+
+debug = True #Global Switch for Debug-Info
+
+#Determine working directory and gridpath
+scriptpath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+gridsubpath = "/Grids/3by4.grid"
+gridpath = scriptpath + gridsubpath
+
+#Debug-Info
+if debug:
+    print("Scriptpath: ")
+    print(scriptpath.encode('utf-8'))
+    print("Gridpath: ")
+    print(gridpath.encode('utf-8'))
 
 
-print("Hello")
+#Read Grid-File as 2-dimensional array 'grid' from 'gridpath'
+with open(gridpath) as gridfile:
+    grid = [line.split() for line in gridfile]
+
+
+#Debug-Info
+if debug:
+    print("Raw-Grid: ")
+    print(grid)
+    print("Grid cleaned-up: ")
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            print(grid[i][j], end=' ')
+        print()
+
